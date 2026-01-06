a3148800adea14be05c8c33883a4df5bd180371f
Ty Jones-Magee <tyree.jones.magee@gmail.com>
2026-01-05 21:48:43 -0500

    Add new print statement about building habits

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
     system.out.println(" This is a new start.");
     system.out.println("Practice makes perfect.");
+        system.out.println("Building the habitis the most important.");
     }
 }
